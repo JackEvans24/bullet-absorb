654ca16b55515d5dc01733cf7d801c8ac8b68421
Jack Evans <jackevans24@gmail.com>
2024-05-29 20:50:58 +0100

    feat: add turret and basic bullet

---
+++ scripts/bullet.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@export var speed = 5
+@export var accuracy = 1.0 / 6.0
+
+func initialise(start_position: Vector3, turret_basis: Basis):
+	basis = turret_basis.orthonormalized()
+	position = start_position
+
+	# add some random rotation to look direction
+	rotate_y(randf_range( - PI * accuracy, PI * accuracy))
+
+func _physics_process(_delta):
+	var forward = -global_transform.basis.z
+	position += forward * speed * _delta
+
+func _on_screen_exited():
+	queue_free()
