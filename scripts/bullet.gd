e0eba2db4fd25babe77236ebda7de914b31bfbe4
Jack Evans <jackevans24@gmail.com>
2024-06-11 16:44:03 +0100

    feat: tween bullet light level so it doesn't pop in/out

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,6 +2,14 @@ class_name Bullet extends Node3D
 
 @export var speed = 5
 @export var accuracy = 1.0 / 6.0
+@export var illuminate_time = 0.2
+@export var illuminate_range = 5
+
+@onready var light: OmniLight3D = $BulletLight
+
+func _ready():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light, "omni_range", illuminate_range, illuminate_time)
 
 func initialise(start_position: Vector3, turret_basis: Basis):
 	basis = turret_basis.orthonormalized()
