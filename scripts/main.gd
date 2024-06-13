82b5e38482f4401f8882fdfd421ac9e1bf1e71f1
Jack Evans <jackevans24@gmail.com>
2024-06-13 18:24:37 +0100

    feat: restart game by pressing fire when dead

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,10 +4,23 @@ extends Node
 @onready var player: Player = $Player
 @onready var turret = $Turret
 
+var is_dead = false
+
 func _ready():
 	turret.set_target(player)
 
+	player.health_changed.connect(_on_player_health_changed)
 	player.health_changed.connect(hud._on_health_changed)
 	player.power_count_changed.connect(hud._on_absorb_count_changed)
 
 	hud._on_health_changed(player.current_health)
+
+func _input(event: InputEvent):
+	if is_dead and event.is_action_pressed("fire"):
+		get_tree().call_group("bullet", "queue_free")
+		get_tree().reload_current_scene()
+
+func _on_player_health_changed(current_health: float):
+	if current_health > 0:
+		return
+	is_dead = true
