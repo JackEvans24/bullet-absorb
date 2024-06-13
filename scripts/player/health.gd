2c82e5a905b7a54377534f43c2e92fc1f2ced29b
Jack Evans <jackevans24@gmail.com>
2024-06-13 17:35:13 +0100

    feat: add recovery time on hurt

--- scripts/player/health.gd
+++ scripts/player/health.gd
@@ -4,9 +4,19 @@ extends Node
 signal damage_taken(damage_taken: float)
 
 @export var starting_health = 5.0
+@export var recovery_time = 1.0
 
 @onready var current_health = starting_health
 
+var is_recovering = false
+
 func take_damage(damage: float):
+	if is_recovering:
+		return
+	is_recovering = true
+
 	current_health = max(0.0, current_health - damage)
 	damage_taken.emit(damage)
+
+	await get_tree().create_timer(recovery_time).timeout
+	is_recovering = false
