9ef29c92ca5a3c95c0ef54c457b3640f8999ecdf
Jack Evans <jackevans24@gmail.com>
2024-06-14 13:53:13 +0100

    feat: hide enemy mesh on death

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -6,6 +6,7 @@ extends Node3D
 @onready var health: Health = $Health
 @onready var hit_detection: Area3D = $HitDetection
 @onready var pivot: Node3D = $Pivot
+@onready var body: MeshInstance3D = $Pivot/Body
 
 func _ready():
 	hit_detection.area_entered.connect(_on_area_entered)
@@ -17,8 +18,13 @@ func _on_area_entered(_area: Area3D):
 func _on_damage_taken(damage_taken: float):
 	if damage_taken > 0:
 		add_hurt_particles()
+	if health.current_health <= 0:
+		die()
 
 func add_hurt_particles():
 	var hurt_particles = hurt_particles_scene.instantiate()
 	add_child(hurt_particles)
 	hurt_particles.position = pivot.position
+
+func die():
+	body.visible = false
