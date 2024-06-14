16ebf4533299a35dbb6e4cb1f5cc7abc0a46bad2
Jack Evans <jackevans24@gmail.com>
2024-06-14 12:11:18 +0100

    chore: replace restart action

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,24 +4,21 @@ extends Node
 @onready var player: Player = $Player
 @onready var turret = $Turret
 
-var is_dead = false
-
 func _ready():
 	turret.set_target(player)
 
 	player.damage_taken.connect(_on_damage_taken)
-	player.died.connect(_on_player_died)
 	player.power_count_changed.connect(hud._on_absorb_count_changed)
 
 	hud._on_health_changed(player.current_health)
 
 func _input(event: InputEvent):
-	if is_dead and event.is_action_pressed("fire"):
-		get_tree().call_group("bullet", "queue_free")
-		get_tree().reload_current_scene()
+	if event.is_action_pressed("restart"):
+		restart_game()
+
+func restart_game():
+	get_tree().call_group("bullet", "queue_free")
+	get_tree().reload_current_scene()
 
 func _on_damage_taken():
 	hud._on_health_changed(player.current_health)
-
-func _on_player_died():
-	is_dead = true
