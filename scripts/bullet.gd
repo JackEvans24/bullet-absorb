ae7ac5747b256516e8f66a5ab5803452ea57c7a8
Jack Evans <jackevans24@gmail.com>
2024-06-17 15:00:04 +0100

    refactor: move offscreen signal hookup to code

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,9 +1,15 @@
-class_name Bullet extends Node3D
+class_name Bullet
+extends Area3D
 
 @export var speed = 5
 @export var accuracy = 1.0 / 6.0
 
 @export var power_scene: PackedScene
+
+@onready var on_screen_notifier: VisibleOnScreenNotifier3D = $OnScreenNotifier
+
+func _ready():
+	on_screen_notifier.screen_exited.connect(_on_screen_exited)
 
 func initialise(turret_basis: Basis):
 	basis = turret_basis.orthonormalized()
@@ -18,11 +24,14 @@ func _on_screen_exited():
 	queue_free()
 
 func destroy():
-	call_deferred("handle_destruction")
+	call_deferred("handle_destruction_with_power")
 
 func handle_destruction():
-	drop_power()
 	queue_free()
+
+func handle_destruction_with_power():
+	drop_power()
+	handle_destruction()
 
 func drop_power():
 	if power_scene == null:
