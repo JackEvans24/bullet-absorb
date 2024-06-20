b8b4a1b7960d49ce2f03e6a638793da1ca128caf
Jack Evans <jackevans24@gmail.com>
2024-06-20 15:27:18 +0100

    feat: add overlay on death

--- scripts/main.gd
+++ scripts/main.gd
@@ -12,6 +12,7 @@ func _ready():
 
 	player.damage_taken.connect(_on_damage_taken)
 	player.power_count_changed.connect(hud._on_absorb_count_changed)
+	player.died.connect(hud._on_player_died)
 
 	hud._on_health_changed(player.current_health)
 
--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -1,11 +1,18 @@
 class_name Hud
 extends Node
 
+@export var death_overlay_timer := 1.5
+
 @onready var health_label: Label = $HealthLabel
 @onready var power_label: Label = $PowerLabel
+@onready var death_overlay: ColorRect = $DeathOverlay
 
 func _on_health_changed(current_health: float):
 	health_label.text = "Health: %.0f" % current_health
 
 func _on_absorb_count_changed(count: int):
 	power_label.text = "Power: %s" % count
+
+func _on_player_died():
+	await get_tree().create_timer(death_overlay_timer).timeout
+	death_overlay.visible = true
