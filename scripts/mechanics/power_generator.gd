c2f7ba6a5e4dd4515c0a9ed7bacbafffe8ce3459
Jack Evans <jackevans24@gmail.com>
2024-06-21 15:26:47 +0100

    feat: add cooldown to power spawn

--- scripts/mechanics/power_generator.gd
+++ scripts/mechanics/power_generator.gd
@@ -2,16 +2,31 @@ class_name PowerGenerator
 extends Node3D
 
 @export var power_scene: PackedScene
+@export var cooldown: float = 0.3
+@export var power_check_time: float = 0.1
 
 @onready var power_check_area: Area3D = $PowerCheckArea
 @onready var spawn_point: Node3D = $SpawnPoint
 
-func _process(delta):
+var can_spawn := true
+
+func _process(_delta):
     if power_check_area.has_overlapping_areas():
         return
+
     spawn_power()
 
 func spawn_power():
+    if not can_spawn:
+        return
+    can_spawn = false
+
+    await get_tree().create_timer(cooldown).timeout
+
     var power = power_scene.instantiate()
     get_tree().root.add_child(power)
     power.global_position = spawn_point.global_position
+
+    await get_tree().create_timer(power_check_time).timeout
+
+    can_spawn = true
