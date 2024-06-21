6b4f48f2e3d7099ddbce64087269b364af929485
Jack Evans <jackevans24@gmail.com>
2024-06-21 16:13:08 +0100

    feat: add variable timer classes

---
+++ scripts/mechanics/shared/variable_timer.gd
@@ -0,0 +1,29 @@
+class_name VariableTimer
+extends Timer
+
+var time_bounds: Array[VariableTimerConfig] = []
+
+var config_index := 0
+var iteration_index := 0
+
+func _ready():
+    var children = get_children()
+    for child in children:
+        time_bounds.push_back(child as VariableTimerConfig)
+
+    wait_time = time_bounds[config_index].duration
+
+    timeout.connect(_on_timeout)
+
+func _on_timeout():
+    iteration_index += 1
+
+    if iteration_index < time_bounds[config_index].iterations:
+        return
+
+    config_index += 1
+    if config_index >= time_bounds.size():
+        config_index = 0
+
+    iteration_index = 0
+    wait_time = time_bounds[config_index].duration
---
+++ scripts/mechanics/shared/variable_timer_config.gd
@@ -0,0 +1,5 @@
+class_name VariableTimerConfig
+extends Node
+
+@export var duration: float = 1.0
+@export var iterations: int = 1
