9c95b80032811734b679bb9b3f82bd0e90c28b1e
Jack Evans <42554897+JackEvans24@users.noreply.github.com>
2024-06-21 15:49:16 +0100

    feat: add power generator which endlessly spawns power (#11)
    * chore: move power ball to power directory
    
    * feat: add power generator which continually spawns power
    
    * feat: add cooldown to power spawn
    
    * feat: add particles on power spawn

---
+++ scripts/mechanics/power_generator.gd
@@ -0,0 +1,35 @@
+class_name PowerGenerator
+extends Node3D
+
+@export var power_scene: PackedScene
+@export var cooldown: float = 0.3
+@export var power_check_time: float = 0.1
+
+@onready var power_check_area: Area3D = $PowerCheckArea
+@onready var spawn_point: Node3D = $SpawnPoint
+@onready var particles: GPUParticles3D = $SpawnParticles
+
+var can_spawn := true
+
+func _process(_delta):
+    if power_check_area.has_overlapping_areas():
+        return
+
+    spawn_power()
+
+func spawn_power():
+    if not can_spawn:
+        return
+    can_spawn = false
+
+    particles.restart()
+
+    await get_tree().create_timer(cooldown).timeout
+
+    var power = power_scene.instantiate()
+    get_tree().root.add_child(power)
+    power.global_position = spawn_point.global_position
+
+    await get_tree().create_timer(power_check_time).timeout
+
+    can_spawn = true
