9fa4c4201ed7c3b6d1b8cb5b83ea09d47d6e5504
Jack Evans <jackevans24@gmail.com>
2024-06-21 11:32:54 +0100

    feat: replace health label with health bar

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -3,12 +3,16 @@ extends Node
 
 @export var death_overlay_timer := 1.5
 
-@onready var health_label: Label = $HealthLabel
+@onready var health_bar: Bar = $HealthBar
 @onready var power_label: Label = $PowerLabel
 @onready var death_overlay: ColorRect = $DeathOverlay
 
+func initialise_max_values(max_health: float):
+	health_bar.max_value = max_health
+	health_bar.update_value(max_health)
+
 func _on_health_changed(current_health: float):
-	health_label.text = "Health: %.0f" % current_health
+	health_bar.update_value(current_health)
 
 func _on_absorb_count_changed(count: int):
 	power_label.text = "Power: %s" % count
