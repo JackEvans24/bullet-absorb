e2e94aaef54a6c392d4edf5880df0a384b4f3d32
Jack Evans <jackevans24@gmail.com>
2024-06-24 15:37:17 +0100

    feat: add behvaiour to fire n bullets in ring around pivot

---
+++ scripts/mechanics/shared/fire_from_ring.gd
@@ -0,0 +1,28 @@
+class_name FireFromRing
+extends Node
+
+@export var pivot_ref: NodePath
+@export var bullet_scene: PackedScene
+
+@export_range(2, 30) var bullet_count := 8
+@export_range(0.0, 5.0) var pivot_offset := 1.0
+
+@onready var pivot: Node3D = get_node(pivot_ref)
+
+func fire():
+	for i in range(bullet_count):
+		generate_bullet(i)
+
+func generate_bullet(i: int):
+	var bullet = bullet_scene.instantiate()
+	add_child(bullet)
+
+	var bullet_rotation = get_rotation(i)
+
+	var offset = Vector3.FORWARD.rotated(Vector3.UP, bullet_rotation) * pivot_offset
+	bullet.global_position = pivot.global_position + offset
+	bullet.initialise(pivot.basis)
+	bullet.rotate_y(bullet_rotation)
+
+func get_rotation(i: int) -> float:
+	return 2.0 * PI * i / bullet_count
