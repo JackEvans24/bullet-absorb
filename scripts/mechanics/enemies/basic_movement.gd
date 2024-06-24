497a2cf06b08378c36c16be9eb6e09f632917542
Jack Evans <jackevans24@gmail.com>
2024-06-24 12:16:40 +0100

    feat: add wall checking to basic movement

--- scripts/mechanics/enemies/basic_movement.gd
+++ scripts/mechanics/enemies/basic_movement.gd
@@ -1,15 +1,21 @@
 class_name BasicMovement
-extends Node
+extends Node3D
 
 @export var speed := 5.0
+@export var wall_check_distance = 2.0
 
 @onready var walk_timer: VariableTimer = $WalkTimer
+@onready var wall_check: RayCast3D = $WallCheck
 
 var is_walking := false
 var movement := Vector3.ZERO
 
 func _ready():
     walk_timer.timeout.connect(change_movement)
+
+func _physics_process(_delta):
+    if wall_check.is_colliding():
+        set_new_movement()
 
 func change_movement():
     if is_walking:
@@ -19,7 +25,12 @@ func change_movement():
     is_walking = !is_walking
 
 func set_new_movement():
-    movement = Vector3.FORWARD.rotated(Vector3.UP, randf_range(0.0, 360.0)) * speed
+    var direction = Vector3.FORWARD.rotated(Vector3.UP, randf_range(0.0, 360.0))
+    movement = direction * speed
+
+    wall_check.target_position = direction * wall_check_distance
+    wall_check.enabled = true
 
 func stop():
     movement = Vector3.ZERO
+    wall_check.enabled = false
