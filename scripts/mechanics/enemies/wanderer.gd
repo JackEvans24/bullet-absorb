e1cd4d053a98443eb4779e828ddffa9972ece12f
Jack Evans <jackevans24@gmail.com>
2024-06-24 11:23:57 +0100

    feat: add wanderer enemy

---
+++ scripts/mechanics/enemies/wanderer.gd
@@ -0,0 +1,18 @@
+class_name Wanderer
+extends Enemy
+
+@onready var look_at_target: LookAtTarget = $LookAtTarget
+@onready var fire: FireFromPoint = $Fire
+
+var target: Node3D:
+	set(value): look_at_target.target = value;
+
+func _ready():
+	super()
+	look_at_target.pivot = pivot
+	fire.pivot = pivot
+
+func die():
+	super()
+	target = null
+	fire.stop()
