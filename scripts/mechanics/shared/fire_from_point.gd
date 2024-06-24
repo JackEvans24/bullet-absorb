f7de90f1fc0ec3d5fe7e7c9b66528a22d6f6cab5
Jack Evans <jackevans24@gmail.com>
2024-06-24 11:23:31 +0100

    refactor: replace internal fire timer with node reference

--- scripts/mechanics/shared/fire_from_point.gd
+++ scripts/mechanics/shared/fire_from_point.gd
@@ -2,27 +2,27 @@ class_name FireFromPoint
 extends Node
 
 @export var fire_point_ref: NodePath
+@export var timer_ref: NodePath
 @export var bullet_scene: PackedScene
 
 var pivot: Node3D
 
 @onready var fire_point: Node3D = get_node(fire_point_ref)
-@onready var timer: Timer = $FireTimer
+@onready var timer: Timer = get_node(timer_ref)
 
 func _ready():
-    timer.timeout.connect(fire)
+	timer.timeout.connect(fire)
 
 func fire():
-    if pivot == null:
-        printerr("PIVOT IS NULL")
-        return
+	if pivot == null:
+		printerr("PIVOT IS NULL")
+		return
 
-    var bullet = bullet_scene.instantiate()
+	var bullet = bullet_scene.instantiate()
+	add_child(bullet)
 
-    bullet.global_position = fire_point.global_position
-    bullet.initialise(pivot.basis)
-
-    add_child(bullet)
+	bullet.global_position = fire_point.global_position
+	bullet.initialise(pivot.basis)
 
 func stop():
-    timer.stop()
+	timer.stop()
