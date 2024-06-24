b3dff2fa2d1e51147f28499af615daa29f86c1b1
Jack Evans <jackevans24@gmail.com>
2024-06-24 15:39:04 +0100

    feat: add rotation to consecutive ring bursts

--- scripts/mechanics/shared/fire_from_ring.gd
+++ scripts/mechanics/shared/fire_from_ring.gd
@@ -6,10 +6,14 @@ extends Node
 
 @export_range(2, 30) var bullet_count := 8
 @export_range(0.0, 5.0) var pivot_offset := 1.0
+@export var rotation_interval := 0.1
 
 @onready var pivot: Node3D = get_node(pivot_ref)
 
+var rotation_offset := 0.0
+
 func fire():
+	rotation_offset += rotation_interval
 	for i in range(bullet_count):
 		generate_bullet(i)
 
@@ -17,7 +21,7 @@ func generate_bullet(i: int):
 	var bullet = bullet_scene.instantiate()
 	add_child(bullet)
 
-	var bullet_rotation = get_rotation(i)
+	var bullet_rotation = get_rotation(i) + rotation_offset
 
 	var offset = Vector3.FORWARD.rotated(Vector3.UP, bullet_rotation) * pivot_offset
 	bullet.global_position = pivot.global_position + offset
