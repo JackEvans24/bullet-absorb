6bef7dde66c657eec6cdeb04b661481a29c4d8ed
Jack Evans <jackevans24@gmail.com>
2024-06-25 11:01:49 +0100

    feat: drop several power with position offsets

--- scripts/mechanics/enemies/enemy.gd
+++ scripts/mechanics/enemies/enemy.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 @export var hurt_particles_scene: PackedScene
 @export var power_scene: PackedScene
+@export_range(0, 20) var power_drop_count := 3
+@export var power_drop_offset = 0.5
 
 @onready var health: Health = $Health
 @onready var knockback: Knockback = $Knockback
@@ -44,9 +46,16 @@ func do_knockback(taken_from: Node3D):
 func die():
 	pivot.visible = false
 	collider.call_deferred("queue_free")
+	drop_all_power()
 
-	if power_scene == null:
+func drop_all_power():
+	if power_scene == null or power_drop_count <= 0:
 		return
+	for i in range(power_drop_count):
+		drop_power()
+
+func drop_power():
 	var power = power_scene.instantiate()
 	get_tree().root.add_child(power)
-	power.global_position = global_position
+	var offset = Vector3.FORWARD.rotated(Vector3.UP, randf_range(0, PI)) * power_drop_offset
+	power.global_position = global_position + offset
