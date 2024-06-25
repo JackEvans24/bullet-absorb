aba83c36a5c1a2efafff5cde5dffcde7fc9fca31
Jack Evans <jackevans24@gmail.com>
2024-06-25 11:49:34 +0100

    feat: add transition to desired position on power drop

--- scripts/mechanics/shared/drop_power.gd
+++ scripts/mechanics/shared/drop_power.gd
@@ -15,4 +15,5 @@ func drop_single_power():
 	var power = power_scene.instantiate()
 	get_tree().root.add_child(power)
 	var offset = Vector3.FORWARD.rotated(Vector3.UP, randf_range(0, PI)) * power_drop_offset
-	power.global_position = global_position + offset
+	power.global_position = global_position
+	power.target_position = global_position + offset
