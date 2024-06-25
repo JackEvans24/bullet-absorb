9ba0b1a2081fa98d232b6d8c9d0a640f6cd51223
Jack Evans <jackevans24@gmail.com>
2024-06-25 11:54:18 +0100

    feat: add slight deviation to drop angle

--- scripts/mechanics/shared/drop_power.gd
+++ scripts/mechanics/shared/drop_power.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var power_scene: PackedScene
 @export_range(0, 20) var power_drop_count := 3
 @export var power_drop_offset = 0.5
+@export var offset_deviation = 0.05
 
 func drop_all_power():
 	if power_scene == null or power_drop_count <= 0:
@@ -18,5 +19,7 @@ func drop_single_power(offset_angle: float):
 	power.global_position = global_position
 
 	print(offset_angle)
-	var offset = Vector3.FORWARD.rotated(Vector3.UP, PI * 2 * offset_angle) * power_drop_offset
+	var deviation = randf_range( - offset_deviation, offset_deviation)
+	var angle = PI * 2 * (offset_angle + deviation)
+	var offset = Vector3.FORWARD.rotated(Vector3.UP, angle) * power_drop_offset
 	power.target_position = global_position + offset
