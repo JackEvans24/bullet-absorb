a74a6b388f805b87d1e126dbf90b5dafccb5e2b0
Jack Evans <jackevans24@gmail.com>
2024-06-26 16:14:41 +0100

    feat: add randomness to variable timer

--- scripts/mechanics/shared/variable_timer_config.gd
+++ scripts/mechanics/shared/variable_timer_config.gd
@@ -2,4 +2,10 @@ class_name VariableTimerConfig
 extends Node
 
 @export var duration: float = 1.0
+@export var duration_randomness: float = 0.2
 @export var iterations: int = 1
+
+func get_duration() -> float:
+    if duration_randomness > 0.0:
+        return randf_range(duration - duration_randomness, duration + duration_randomness)
+    return duration
