ad888e6aa8bf9b6564876627380568d04661a15a
Jack Evans <jackevans24@gmail.com>
2024-06-26 12:20:23 +0100

    feat: track whether entry is first entry

--- scripts/mechanics/rooms/room.gd
+++ scripts/mechanics/rooms/room.gd
@@ -6,10 +6,18 @@ extends Node3D
 @onready var boundary: RoomBoundary = $Boundary
 @onready var player_detection: Area3D = $PlayerDetection
 
+var has_previously_entered := false
+
 func _ready():
 	boundary.set_doors(config.doors)
 	player_detection.body_entered.connect(_on_player_entered)
 
 func _on_player_entered(_body: Node3D):
-	player_detection.body_entered.disconnect(_on_player_entered)
-	boundary.call_deferred("close_all_doors")
+	if has_previously_entered:
+		return
+	has_previously_entered = true
+
+	call_deferred("on_first_entry")
+
+func on_first_entry():
+	boundary.close_all_doors()
