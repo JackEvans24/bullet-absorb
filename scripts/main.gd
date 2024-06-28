bff8e35c7b57159d6247e1db366daf67678bdcf2
Jack Evans <jackevans24@gmail.com>
2024-06-28 12:26:13 +0100

    feat: add room controller to scene and hook up to screen shake

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var hud: Hud = $HUD
 @onready var player: Player = $Player
+@onready var rooms: RoomController = $World/Rooms
 @onready var cameras: CameraController = $Cameras
 @onready var hit_stop: HitStop = $HitStop
 
@@ -12,6 +13,9 @@ func _ready():
 	player.power_count_changed.connect(hud._on_absorb_count_changed)
 	player.can_dash_changed.connect(hud._on_can_dash_changed)
 	player.died.connect(hud._on_player_died)
+
+	rooms.room_activated.connect(_on_room_state_changed)
+	rooms.room_completed.connect(_on_room_state_changed)
 
 	hud.initialise_max_values(player.max_health, player.max_power)
 	hud._on_health_changed(player.current_health)
@@ -28,3 +32,6 @@ func _on_damage_taken():
 	hit_stop.freeze()
 	cameras.add_impulse()
 	hud._on_health_changed(player.current_health)
+
+func _on_room_state_changed():
+	cameras.add_impulse(0.5)
