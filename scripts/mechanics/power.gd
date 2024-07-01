ae4ba346aac9f369c8106fc130402dcf68a46f4d
Jack Evans <jackevans24@gmail.com>
2024-07-01 11:11:32 +0100

    feat: add minor delay to power attraction behaviour

--- scripts/mechanics/power.gd
+++ scripts/mechanics/power.gd
@@ -2,14 +2,30 @@ class_name Power
 extends Node3D
 
 @export var wall_check_distance := 0.3
+@export var attraction_delay := 0.3
 
 @onready var attraction_area = $PlayerAttractionArea
 @onready var follow_body: FollowBody3D = $FollowBody
 @onready var smooth_movement: SmoothMovement = $SmoothMovement
 @onready var wall_check: RayCast3D = $WallCheck
 
+var can_attract = false
+var target: Node3D
+
 func _ready():
 	attraction_area.body_entered.connect(_on_body_entered)
+
+	await get_tree().create_timer(attraction_delay).timeout
+	can_attract = true
+
+func _process(_delta):
+	if target == null or not can_attract:
+		return
+
+	follow_body.target = target
+	target = null
+	smooth_movement.clear_target()
+	wall_check.enabled = false
 
 func _physics_process(_delta):
 	if follow_body.target != null:
@@ -23,9 +39,7 @@ func _physics_process(_delta):
 	translate(smooth_movement.movement)
 
 func _on_body_entered(body: Node3D):
-	follow_body.target = body
-	smooth_movement.clear_target()
-	wall_check.enabled = false
+	target = body
 
 func set_target_position(target_position: Vector3):
 	var direction = target_position - global_position
