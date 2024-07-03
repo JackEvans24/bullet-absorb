4a47246cba001205ccd15fae67ba467d8ceff477
Jack Evans <jackevans24@gmail.com>
2024-07-03 12:48:04 +0100

    feat: add intro animation to turret

--- scripts/mechanics/enemies/turret.gd
+++ scripts/mechanics/enemies/turret.gd
@@ -2,6 +2,7 @@ class_name Turret
 extends Enemy
 
 @onready var swivel: Node3D = $Pivot/TurretSwivel
+@onready var spawn_tube: SpawnTube = $Pivot/SpawnTube
 @onready var look_at_target: LookAtTarget = $LookAtTarget
 @onready var fire: FireFromPoint = $Fire
 @onready var fire_timer: VariableTimer = $FireTimer
@@ -11,15 +12,20 @@ func _ready():
 	fire.pivot = swivel
 	fire_timer.named_timeout.connect(_on_behaviour_timer_timeout)
 
+func _on_intro_animation_start():
+	spawn_tube.play_animation()
+
+func _on_intro_animation_complete():
 	call_deferred("set_hit_detection")
+	spawn_tube.call_deferred("queue_free")
 	fire_timer.restart()
 
 func set_target(target: Node3D):
 	look_at_target.target = target
 
 func _on_behaviour_timer_timeout(timer_name: String):
-	match timer_name.to_lower():
-		"fire": fire.fire()
+	if timer_name.to_lower() == "fire":
+		fire.fire()
 
 func die():
 	super()
