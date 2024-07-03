6d4b10f84f613bc8355d916919c2dce95c0a2b48
Jack Evans <jackevans24@gmail.com>
2024-07-03 12:08:37 +0100

    feat: use turret models in turret scene

--- scripts/mechanics/enemies/turret.gd
+++ scripts/mechanics/enemies/turret.gd
@@ -1,15 +1,18 @@
 class_name Turret
 extends Enemy
 
+@onready var swivel: Node3D = $Pivot/TurretSwivel
 @onready var look_at_target: LookAtTarget = $LookAtTarget
 @onready var fire: FireFromPoint = $Fire
 @onready var fire_timer: Timer = $FireTimer
 
 func _ready():
-	super()
-	look_at_target.pivot = pivot
-	fire.pivot = pivot
+	look_at_target.pivot = swivel
+	fire.pivot = swivel
 	fire_timer.timeout.connect(fire.fire)
+
+	call_deferred("set_hit_detection")
+	fire_timer.restart()
 
 func set_target(target: Node3D):
 	look_at_target.target = target
