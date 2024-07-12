a7666c3a67c407184a7f2b365b25ec4e7317bed2
Jack Evans <42554897+JackEvans24@users.noreply.github.com>
2024-07-12 16:37:38 +0100

    feat: add configuration for waves of enemies (#24)
    * refactor: rename doors to be less confusing
    
    * chore: move room items in hierarchy
    
    * refactor: rework room config to allow for waves
    
    * chore: reset positions of room enemies
    
    * chore: fix wanderer positions in room
    
    * chore: add wanderer room to main for testing
    
    * feat: add instantiation delay to room items
    
    * feat: add wave configuration
    
    * feat: add waves to wanderer room

--- scripts/mechanics/rooms/room.gd
+++ scripts/mechanics/rooms/room.gd
@@ -3,18 +3,17 @@ extends Node3D
 
 signal doors_changed
 
-@export var active_config: RoomData
-@export var inactive_config: RoomData
-@export var completed_config: RoomData
+@export var data: RoomData
 
 @onready var boundary: RoomBoundary = $Boundary
 @onready var player_detection: Area3D = $PlayerDetection
 
 var player: Node3D
 var enemy_count = 0
+var wave_index = 0
 
 func _ready():
-	set_room_configuration(inactive_config)
+	set_doors(data.untouched_doors)
 	player_detection.body_entered.connect(_on_player_entered)
 
 func _on_player_entered(body: Node3D):
@@ -25,16 +24,10 @@ func _on_player_entered(body: Node3D):
 	call_deferred("on_first_entry")
 
 func on_first_entry():
-	set_room_configuration(active_config)
+	close_all_doors()
+	set_room_configuration(data.waves[wave_index])
 
-func set_room_configuration(config: RoomData):
-	var doors_set = set_doors(config)
-	if doors_set:
-		doors_changed.emit()
-
-	if config.spawn_delay > 0.0:
-		await get_tree().create_timer(config.spawn_delay).timeout
-
+func set_room_configuration(config: RoomConfiguration):
 	for enemy_config in config.enemies:
 		add_enemy(enemy_config)
 		enemy_count += 1
@@ -42,11 +35,14 @@ func set_room_configuration(config: RoomData):
 		add_item(item_config)
 
 func add_enemy(enemy_config: RoomItem):
-	var enemy = add_item(enemy_config) as Enemy
+	var enemy = await add_item(enemy_config) as Enemy
 	enemy.set_target(player)
 	enemy.died.connect(_on_enemy_died)
 
 func add_item(item_config: RoomItem) -> Node3D:
+	if item_config.delay > 0.0:
+		await get_tree().create_timer(item_config.delay).timeout
+
 	var item = item_config.scene.instantiate() as Node3D
 	add_child(item)
 	item.position = item_config.position
@@ -58,13 +54,22 @@ func _on_enemy_died():
 
 	enemy_count -= 1
 	if enemy_count == 0:
-		call_deferred("on_room_complete")
+		call_deferred("on_wave_complete")
 
-func on_room_complete():
-	set_room_configuration(completed_config)
+func on_wave_complete():
+	wave_index += 1
+	if wave_index < len(data.waves):
+		set_room_configuration(data.waves[wave_index])
+		return
 
-func set_doors(config: RoomData) -> bool:
-	if not boundary.doors_need_changing(config.doors):
-		return false
-	boundary.set_doors(config.doors)
-	return true
+	set_doors(data.completed_doors)
+	set_room_configuration(data.completed_room)
+
+func close_all_doors():
+	set_doors(0)
+
+func set_doors(doors: int):
+	if not boundary.doors_need_changing(doors):
+		return
+	boundary.set_doors(doors)
+	doors_changed.emit()
