c471dfe0f782c05bd09b891cefac703e6e5aa372
Jack Evans <jackevans24@gmail.com>
2024-07-12 16:22:13 +0100

    refactor: rename doors to be less confusing

--- scripts/mechanics/rooms/room_doors.gd
+++ scripts/mechanics/rooms/room_doors.gd
@@ -1,26 +1,26 @@
 class_name RoomBoundary
 extends Node3D
 
-enum Walls {FORWARD = 1, BACK = 2, LEFT = 4, RIGHT = 8}
+enum Walls {NORTH = 1, SOUTH = 2, EAST = 4, WEST = 8}
 
-@onready var front_door: Door = $Front/Door
-@onready var back_door: Door = $Back/Door
-@onready var left_door: Door = $Left/Door
-@onready var right_door: Door = $Right/Door
+@onready var north_door: Door = $North/Door
+@onready var south_door: Door = $South/Door
+@onready var east_door: Door = $East/Door
+@onready var west_door: Door = $West/Door
 
 func doors_need_changing(doors: int) -> bool:
-	if (doors&Walls.FORWARD != 0) == front_door.is_closed:
+	if (doors&Walls.NORTH != 0) == north_door.is_closed:
 		return true
-	if (doors&Walls.BACK != 0) == back_door.is_closed:
+	if (doors&Walls.SOUTH != 0) == south_door.is_closed:
 		return true
-	if (doors&Walls.LEFT != 0) == left_door.is_closed:
+	if (doors&Walls.WEST != 0) == west_door.is_closed:
 		return true
-	if (doors&Walls.RIGHT != 0) == right_door.is_closed:
+	if (doors&Walls.EAST != 0) == east_door.is_closed:
 		return true
 	return false
 
 func set_doors(doors: int):
-	front_door.set_closed(doors&Walls.FORWARD == 0)
-	back_door.set_closed(doors&Walls.BACK == 0)
-	left_door.set_closed(doors&Walls.LEFT == 0)
-	right_door.set_closed(doors&Walls.RIGHT == 0)
+	north_door.set_closed(doors&Walls.NORTH == 0)
+	south_door.set_closed(doors&Walls.SOUTH == 0)
+	west_door.set_closed(doors&Walls.WEST == 0)
+	east_door.set_closed(doors&Walls.EAST == 0)
