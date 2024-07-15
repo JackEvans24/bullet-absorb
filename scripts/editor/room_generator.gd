48d01e00d74975d537d116e7cbc5c03a5a671db6
Jack Evans <jackevans24@gmail.com>
2024-07-15 17:05:23 +0100

    feat: add door generation to room generator

--- scripts/editor/room_generator.gd
+++ scripts/editor/room_generator.gd
@@ -1,7 +1,7 @@
 @tool
 
 class_name RoomGenerator
-extends Node
+extends Node3D
 
 const WALL_ITEM := 0
 const FLOOR_ITEM := 1
@@ -16,15 +16,29 @@ const FLOOR_ITEM := 1
 @export var detection_offset := 2.0
 @export var detection_height := 2.0
 
+@export_group("Doors")
+@export var door_scene: PackedScene
+@export_flags("NORTH", "SOUTH", "EAST", "WEST") var doors := 0
+
+var grid: GridMap
+var boundary: RoomBoundary
+
 func start_generation(_value: bool):
 	if not Engine.is_editor_hint():
 		return
 
-	var grid: GridMap = $Room/Grid
+	# Set references
+	grid = $Room/Grid
+	boundary = $Room/Boundary
+
+	# Clear existing grid
+	for child in grid.get_children():
+		print(child.name)
+		grid.remove_child(child)
+		child.queue_free()
 	grid.clear()
 
-	# Calculate wall positions
-	# Set gridmap cells
+	# Build walls
 	var pos := Vector3(0, 0, 0)
 	for z in range( - depth, depth):
 		for x in range( - width, width):
@@ -37,6 +51,31 @@ func start_generation(_value: bool):
 			else:
 				grid.set_cell_item(pos, FLOOR_ITEM)
 
+	# Set doors
+	if (doors&1 != 0):
+		boundary.north_door = add_door(Vector3(0, 0, -depth), Vector3.RIGHT, "NorthDoor")
+	if (doors&2 != 0):
+		boundary.south_door = add_door(Vector3(0, 0, depth - 1), Vector3.RIGHT, "SouthDoor")
+	if (doors&4 != 0):
+		boundary.east_door = add_door(Vector3(width - 1, 0, 0), Vector3.BACK, "EastDoor", PI / 2.0)
+	if (doors&8 != 0):
+		boundary.west_door = add_door(Vector3( - width, 0, 0), Vector3.BACK, "WestDoor", PI / 2.0)
+
+	# Set player detection space
 	var detection_shape: CollisionShape3D = $Room/PlayerDetection/Shape
 	var box: BoxShape3D = detection_shape.shape
-	box.size = Vector3(width - detection_offset, detection_height, depth - detection_offset)
+	box.size = Vector3((width * 2) - detection_offset, detection_height, (depth * 2) - detection_offset)
+
+func add_door(door_position: Vector3, grid_direction: Vector3, door_name: String, door_rotation: float=0.0) -> Node3D:
+	for x in range( - 2, 2):
+		var cell = door_position + (grid_direction * x)
+		print(cell)
+		grid.set_cell_item(cell, GridMap.INVALID_CELL_ITEM)
+
+	var door: Node3D = door_scene.instantiate()
+	grid.add_child(door)
+	door.name = door_name
+	door.position = door_position
+	door.rotate_y(door_rotation)
+
+	return door
--- scripts/mechanics/rooms/room_doors.gd
+++ scripts/mechanics/rooms/room_doors.gd
@@ -1,17 +1,20 @@
 class_name RoomBoundary
-extends Node
+extends Node3D
 
 enum Walls {NORTH = 1, SOUTH = 2, EAST = 4, WEST = 8}
 
-@export var north_door_ref: NodePath
-@export var south_door_ref: NodePath
-@export var east_door_ref: NodePath
-@export var west_door_ref: NodePath
+@export var north_door: Door
+@export var south_door: Door
+@export var east_door: Door
+@export var west_door: Door
 
-@onready var north_door: Door = get_node(north_door_ref)
-@onready var south_door: Door = get_node(south_door_ref)
-@onready var east_door: Door = get_node(east_door_ref)
-@onready var west_door: Door = get_node(west_door_ref)
+func clear_doors():
+	if not Engine.is_editor_hint():
+		return
+	north_door = null
+	south_door = null
+	east_door = null
+	west_door = null
 
 func doors_need_changing(doors: int) -> bool:
 	if north_door != null&&(doors&Walls.NORTH != 0) == north_door.is_closed:
