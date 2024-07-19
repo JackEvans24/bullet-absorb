67fae3d2eab550cbb222e9dfa7d54f5f9a3429c7
Jack Evans <jackevans24@gmail.com>
2024-07-19 16:20:17 +0100

    feat: add script feature to utilise save system

--- scripts/save/save_game.gd
+++ scripts/save/save_game.gd
@@ -7,14 +7,20 @@ var SAVE_FILE_PATH := "user://bullet_absorb.save"
 var data := SaveGameData.new()
 
 func save():
+	if not OS.has_feature(GameFeatures.SAVE_SYSTEM):
+		return
+
 	var save_file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
 	var serialised_data = JSON.stringify(data.to_dictionary())
 	save_file.store_string(serialised_data)
 
 func load():
+	if not OS.has_feature(GameFeatures.SAVE_SYSTEM):
+		overwrite_start_room()
+		return
+
 	if not FileAccess.file_exists(SAVE_FILE_PATH):
 		print("File not found")
-		overwrite_start_room()
 		return
 
 	var save_file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
@@ -23,15 +29,11 @@ func load():
 	var parse_result = json.parse(save_file.get_as_text())
 	if parse_result != OK:
 		print("JSON parse error: ", json.get_error_message())
-		overwrite_start_room()
 		return
 
 	data.read(json.data)
-	overwrite_start_room()
 
 func overwrite_start_room():
-	if not OS.has_feature("debug"):
-		return
 	if not start_room_override:
 		return
 	if not data.completed_rooms.has(start_room_override):
