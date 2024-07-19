29e68f9c91bf24e2c0fca9f038708335f27371ad
Jack Evans <jackevans24@gmail.com>
2024-07-19 14:01:40 +0100

    feat: add room name and store rooms by name

--- scripts/mechanics/rooms/room_controller.gd
+++ scripts/mechanics/rooms/room_controller.gd
@@ -4,15 +4,21 @@ signal doors_changed
 
 @export var room_item_lookup: RoomItemLookup
 
-var rooms: Array[Room]
+var rooms: Dictionary
 
 func _ready():
 	var children = get_children()
 	for child in children:
 		var room = child as Room
+
+		if rooms.has(room.data.room_name):
+			printerr("Duplicate room name found whilst processing ", room.name, ": ", room.data.room_name)
+			continue
+
 		room.room_item_lookup = room_item_lookup
 		room.doors_changed.connect(_on_room_doors_changed)
-		rooms.push_back(child as Room)
+
+		rooms[room.data.room_name] = room
 
 func _on_room_doors_changed():
 	doors_changed.emit()
