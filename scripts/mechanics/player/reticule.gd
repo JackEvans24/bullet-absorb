7b8462d011f63ccf57a347a5832945bfe414b8dc
Jack Evans <jackevans24@gmail.com>
2024-07-22 14:00:09 +0100

    feat: add minimum reticule distance

--- scripts/mechanics/player/reticule.gd
+++ scripts/mechanics/player/reticule.gd
@@ -1,6 +1,7 @@
 class_name Reticule
 extends Node3D
 
+@export var min_reticule_distance := 1.0
 @export var max_reticule_distance := 6.0
 @export var line_offset := Vector3(0, 0, 0.5)
 @export_range(0.01, 1.0) var line_length_multiplier := 0.5
@@ -16,7 +17,10 @@ func _physics_process(_delta):
         set_mesh_positions(Vector3.FORWARD * max_reticule_distance)
         return
 
-    set_mesh_positions(to_local(distance_check.get_collision_point()))
+    var collision_point = to_local(distance_check.get_collision_point())
+    if collision_point.length() < min_reticule_distance:
+        collision_point = collision_point.normalized() * min_reticule_distance
+    set_mesh_positions(collision_point)
 
 func set_mesh_positions(reticule_position: Vector3):
     reticule_mesh.position = reticule_position
