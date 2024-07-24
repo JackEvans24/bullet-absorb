1a3720491fe502f9aee4b0a136b235b4a973133e
Jack Evans <jackevans24@gmail.com>
2024-07-24 12:53:32 +0100

    feat: add max power reward

--- scripts/data/player_stats.gd
+++ scripts/data/player_stats.gd
@@ -1,7 +1,7 @@
 class_name PlayerStats extends Resource
 
 @export var max_health := 5
-@export var max_power := 10
+@export var max_power := 10.0
 @export var power_conversion := 1.0
 @export var fire_power_consumption_modifier := 1.0
 @export var fire_cooldown_modifier := 2.0
--- scripts/data/rewards/base_reward.gd
+++ scripts/data/rewards/base_reward.gd
@@ -1,6 +1,15 @@
 class_name Reward extends Resource
 
-enum RewardType {None, Health}
+enum RewardType {
+    None,
+    Health,
+    MaxPower,
+    # PowerConversion,
+    # FireConsumption,
+    # FireCooldown,
+    # AbsorbWindup,
+    # AbsorbArea
+}
 
 func upgrade(_player: Player):
     printerr("Using base reward in game, use inherited class instead")
