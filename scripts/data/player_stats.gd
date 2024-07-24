fa3b9fd6c6957201e891031f1a235876909a9569
Jack Evans <jackevans24@gmail.com>
2024-07-24 15:13:49 +0100

    chore: reduce base stats

--- scripts/data/player_stats.gd
+++ scripts/data/player_stats.gd
@@ -4,8 +4,8 @@ class_name PlayerStats extends Resource
 @export var max_power := 10.0
 @export var power_conversion := 1.0
 @export var fire_power_consumption_modifier := 1.0
-@export var fire_cooldown_modifier := 2.0
-@export var absorb_windup_modifier := 1.5
+@export var fire_cooldown_modifier := 1.0
+@export var absorb_windup_modifier := 1.0
 @export var absorb_area := 1.0
 
 var fire_power_consumption: float:
