df5014f4182271abdeb48c15c107417d357fb27b
Jack Evans <jackevans24@gmail.com>
2024-07-30 14:23:10 +0100

    chore: clean up unused prop

--- scripts/data/rooms/boss_data.gd
+++ scripts/data/rooms/boss_data.gd
@@ -1,7 +1,6 @@
 class_name BossData
 extends Resource
 
-@export var title: String = "New Boss"
 @export var max_health: float = 20.0
 @export var power_count: int = 20
 @export var boss_scene: PackedScene
