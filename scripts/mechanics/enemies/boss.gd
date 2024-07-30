5a4826e673fef4bcfa03f4bc22351b415253c129
Jack Evans <jackevans24@gmail.com>
2024-07-30 14:17:20 +0100

    fix: fix overlapping damage taken functions

--- scripts/mechanics/enemies/boss.gd
+++ scripts/mechanics/enemies/boss.gd
@@ -5,9 +5,6 @@ signal health_changed(current_health: float)
 
 @export var title := "Boss"
 
-func _ready():
-    super()
-    health.damage_taken.connect(_on_damage_taken)
-
-func _on_damage_taken(_damage_taken: float, _taken_from: Node3D):
+func _on_damage_taken(damage_taken: float, taken_from: Node3D):
     health_changed.emit(health.current_health)
+    super(damage_taken, taken_from)
