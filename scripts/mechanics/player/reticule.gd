c355f62243cab358612cf6b78879075ef7a9b7c7
Jack Evans <jackevans24@gmail.com>
2024-08-02 12:55:48 +0100

    refactor: reduce vector creations and assignments

--- scripts/mechanics/player/reticule.gd
+++ scripts/mechanics/player/reticule.gd
@@ -18,15 +18,14 @@ func _ready():
 
 func _physics_process(_delta):
     if not distance_check.is_colliding():
-        set_mesh_positions(Vector3.FORWARD, max_reticule_distance, max_look_at_distance)
+        set_mesh_positions(max_reticule_distance, max_look_at_distance)
         return
 
-    var collision_point = to_local(distance_check.get_collision_point())
-    var collision_distance = collision_point.length()
+    var collision_distance = to_local(distance_check.get_collision_point()).length()
     var reticule_distance = clampf(collision_distance, min_reticule_distance, max_reticule_distance)
     var look_at_distance = clampf(collision_distance, min_look_at_distance, max_look_at_distance)
-    set_mesh_positions(collision_point.normalized(), reticule_distance, look_at_distance)
+    set_mesh_positions(reticule_distance, look_at_distance)
 
-func set_mesh_positions(direction: Vector3, reticule_distance: float, look_at_distance: float):
-    reticule_mesh.position = direction * reticule_distance
-    look_at_node.position = direction * look_at_distance
+func set_mesh_positions(reticule_distance: float, look_at_distance: float):
+    reticule_mesh.position.z = reticule_distance
+    look_at_node.position.z = look_at_distance
