acfee7bf73de4cca4aa588807105826f3a09ad8c
Jack Evans <jackevans24@gmail.com>
2024-08-02 13:12:21 +0100

    feat: allow variable aim distances for mouse

--- scripts/mechanics/player/reticule.gd
+++ scripts/mechanics/player/reticule.gd
@@ -13,17 +13,19 @@ extends Node3D
 @onready var look_at_node: Node3D = $LookAt
 @onready var distance_check: RayCast3D = $DistanceCheck
 
+var aim_distance := 1.0
+
 func _ready():
     distance_check.target_position = Vector3.FORWARD * max_reticule_distance
 
 func _physics_process(_delta):
-    if not distance_check.is_colliding():
-        set_mesh_positions(max_reticule_distance, max_look_at_distance)
-        return
+    var target_distance = aim_distance
+    if distance_check.is_colliding():
+        var collision_distance = to_local(distance_check.get_collision_point()).length()
+        target_distance = min(target_distance, collision_distance)
 
-    var collision_distance = to_local(distance_check.get_collision_point()).length()
-    var reticule_distance = clampf(collision_distance, min_reticule_distance, max_reticule_distance)
-    var look_at_distance = clampf(collision_distance, min_look_at_distance, max_look_at_distance)
+    var reticule_distance = clampf(target_distance, min_reticule_distance, max_reticule_distance)
+    var look_at_distance = clampf(target_distance, min_look_at_distance, max_look_at_distance)
     set_mesh_positions(reticule_distance, look_at_distance)
 
 func set_mesh_positions(reticule_distance: float, look_at_distance: float):
