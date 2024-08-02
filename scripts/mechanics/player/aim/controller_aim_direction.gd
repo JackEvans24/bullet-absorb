7a7aabe22d1aa02bc53378d7e16944309ac5ece7
Jack Evans <jackevans24@gmail.com>
2024-08-02 13:00:28 +0100

    refactor: don't normalise controller aim vector

--- scripts/mechanics/player/aim/controller_aim_direction.gd
+++ scripts/mechanics/player/aim/controller_aim_direction.gd
@@ -3,4 +3,4 @@ extends AimDirection
 
 func get_aim_direction() -> Vector3:
 	var input_dir = Input.get_vector("aim_left", "aim_right", "aim_forward", "aim_back")
-	return Vector3(input_dir.x, 0, input_dir.y).normalized()
+	return Vector3(input_dir.x, 0, input_dir.y)
