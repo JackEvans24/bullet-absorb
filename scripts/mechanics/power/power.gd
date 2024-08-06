1c37cfe4f6fbd161483e43e557322c6935cf081c
Jack Evans <jackevans24@gmail.com>
2024-08-06 14:35:59 +0100

    feat: add power ball collection sfx

--- scripts/mechanics/power/power.gd
+++ scripts/mechanics/power/power.gd
@@ -6,16 +6,20 @@ signal absorbed
 @export var wall_check_distance := 0.3
 @export var attraction_delay := 0.3
 @export var trigger_power_handler := true
+@onready var destroy_offset := 0.5
 
+@onready var pivot: Node3D = $Pivot
 @onready var attraction_area: Area3D = $PlayerAttractionArea
 @onready var collision_area: Area3D = $PlayerCollisionArea
 @onready var follow_body: FollowBody3D = $FollowBody
 @onready var smooth_movement: SmoothMovement = $SmoothMovement
 @onready var wall_check: RayCast3D = $WallCheck
+@onready var sfx: SoundBankUncached = $SoundBank
 
 var current_attraction_timer := 0.0
 var can_attract = false
 var target: Node3D
+var dead := false
 
 func _ready():
 	attraction_area.body_entered.connect(_on_body_entered_attraction)
@@ -59,12 +63,25 @@ func _on_body_entered_attraction(body: Node3D):
 		target = body
 
 func _on_body_entered_collision(body: Node3D):
+	if dead:
+		return
 	if trigger_power_handler and body.has_node("PowerHitHandler"):
 		body.get_node("PowerHitHandler").trigger(self)
 
 	absorbed.emit()
 
-	call_deferred("queue_free")
+	call_deferred("handle_destruction")
+
+func handle_destruction():
+	if dead:
+		return
+	dead = true
+
+	pivot.visible = false
+	sfx.play("Splash")
+
+	await get_tree().create_timer(destroy_offset).timeout
+	queue_free()
 
 func set_target_position(target_position: Vector3):
 	var direction = target_position - global_position
