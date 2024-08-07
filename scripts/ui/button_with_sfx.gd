dff9881a740d853708408a1dc12618ad3733f9bf
Jack Evans <jackevans24@gmail.com>
2024-08-07 15:47:25 +0100

    feat: add pressed noise to default button

--- scripts/ui/button_with_sfx.gd
+++ scripts/ui/button_with_sfx.gd
@@ -2,10 +2,15 @@ class_name ButtonWithSfx
 extends Button
 
 @onready var select_sfx: FmodEventEmitter3D = $SelectSFX
+@onready var press_sfx: FmodEventEmitter3D = $PressSFX
 
 func _ready():
     focus_entered.connect(play_selected_sfx)
     mouse_entered.connect(play_selected_sfx)
+    pressed.connect(play_pressed_sfx)
 
 func play_selected_sfx():
     select_sfx.play()
+
+func play_pressed_sfx():
+    press_sfx.play()
