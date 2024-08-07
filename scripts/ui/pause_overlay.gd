4a8407789e539af6c151674388500ed7cfaf6419
Jack Evans <jackevans24@gmail.com>
2024-08-07 13:35:01 +0100

    chore: replace spaces with tabs in file

--- scripts/ui/pause_overlay.gd
+++ scripts/ui/pause_overlay.gd
@@ -6,10 +6,10 @@ signal restart_requested
 @onready var restart_button: Button = $RestartButton
 
 func _ready():
-    restart_button.pressed.connect(_on_restart_pressed)
+	restart_button.pressed.connect(_on_restart_pressed)
 
 func set_active():
-    restart_button.grab_focus()
+	restart_button.grab_focus()
 
 func _on_restart_pressed():
-    restart_requested.emit()
+	restart_requested.emit()
