2eba2bc106a87e14f0a3ada8ef03a6646bfd0f44
Jack Evans <jackevans24@gmail.com>
2024-08-20 16:42:53 +0100

    feat: update door and door offsets with new models

--- scripts/editor/room_generator.gd
+++ scripts/editor/room_generator.gd
@@ -5,7 +5,6 @@ extends EditorScript
 
 const WALL_ITEM := 0
 const FLOOR_ITEM := 1
-const DOOR_SUPPORT_ITEM := 2
 const INVISIBLE_WALL_ITEM := 3
 
 enum DoorDirection {North = 0, East = 1, South = 2, West = 3}
@@ -15,14 +14,13 @@ const ROT_NORTH := 10
 const ROT_EAST := 22
 const ROT_WEST := 16
 
-const HORIZONTAL_DOOR_OFFSET := Vector3(0, 0, 0.5)
-const VERTICAL_DOOR_OFFSET := Vector3(0.5, 0, 0)
+var DOOR_GRID_OFFSETS: Dictionary
 
 var doorway_rotations: Array[int] = [ROT_SOUTH, ROT_EAST, ROT_NORTH, ROT_WEST]
 
 @export_group("Size")
-@export var depth := 10
-@export var width := 10
+@export var depth := 8
+@export var width := 12
 
 @export_group("Detection")
 @export var detection_offset := 4.0
@@ -31,8 +29,8 @@ var doorway_rotations: Array[int] = [ROT_SOUTH, ROT_EAST, ROT_NORTH, ROT_WEST]
 @export_group("Doors")
 @export var door_scene: PackedScene = preload("res://prefabs/room_items/door.tscn")
 @export_flags("NORTH", "SOUTH", "EAST", "WEST") var start_doors := 2
-@export_flags("NORTH", "SOUTH", "EAST", "WEST") var end_doors := 14
-@export var door_offsets: Vector4 = Vector4(0, 0, 4, -4)
+@export_flags("NORTH", "SOUTH", "EAST", "WEST") var end_doors := 15
+@export var door_offsets: Vector4 = Vector4(0, 0, 0, 0)
 
 var scene: Node
 var room: Node3D
@@ -42,7 +40,15 @@ var boundary: RoomBoundary
 func _run():
 	if not Engine.is_editor_hint():
 		return
+	if not DOOR_GRID_OFFSETS:
+		build_door_offset_dict()
 	generate_room()
+
+func build_door_offset_dict():
+	DOOR_GRID_OFFSETS[DoorDirection.North] = Vector3(0, 0, 1)
+	DOOR_GRID_OFFSETS[DoorDirection.East] = Vector3(0, 0, 0)
+	DOOR_GRID_OFFSETS[DoorDirection.South] = Vector3(0, 0, 0)
+	DOOR_GRID_OFFSETS[DoorDirection.West] = Vector3(1, 0, 0)
 
 func generate_room():
 	# Set references
@@ -105,28 +111,14 @@ func clear_grid(_value: bool):
 	grid.clear()
 
 func add_door(door_position: Vector3, door_rotation_index: DoorDirection, offset: float) -> Node3D:
-	var is_horizontal_door = door_rotation_index % 2 == 0
-	var grid_direction := Vector3.RIGHT if is_horizontal_door else Vector3.BACK
-
-	for x in range(offset - 3, offset + 3):
-		var cell = door_position + (grid_direction * x)
-		if x == (offset - 3):
-			grid.set_cell_item(cell, DOOR_SUPPORT_ITEM, ROT_NORTH if is_horizontal_door else ROT_WEST)
-		elif x == offset + 2:
-			var item = DOOR_SUPPORT_ITEM if is_horizontal_door else INVISIBLE_WALL_ITEM
-			grid.set_cell_item(cell, item, ROT_SOUTH if is_horizontal_door else ROT_EAST)
-		else:
-			grid.set_cell_item(cell, FLOOR_ITEM)
-
 	var door: Node3D = door_scene.instantiate()
 	boundary.add_child(door)
 	var door_name = "Door%s" % DoorDirection.keys()[door_rotation_index]
 	door.name = door_name
 	door.owner = scene
 
-	var door_cell_offset := HORIZONTAL_DOOR_OFFSET if is_horizontal_door else VERTICAL_DOOR_OFFSET
-	var door_placement_offset := (VERTICAL_DOOR_OFFSET if is_horizontal_door else HORIZONTAL_DOOR_OFFSET) * 2 * offset
-	door.position = door_position + door_cell_offset + door_placement_offset
+	var door_cell_offset: Vector3 = DOOR_GRID_OFFSETS[door_rotation_index]
+	door.position = door_position + door_cell_offset
 	door.rotate_y(door_rotation_index * PI / 2.0)
 
 	return door
--- scripts/mechanics/rooms/door.gd
+++ scripts/mechanics/rooms/door.gd
@@ -1,9 +1,12 @@
 class_name Door extends StaticBody3D
 
+@export var door_path: NodePath
+
+@onready var door_mesh: Node3D = get_node(door_path)
 @onready var collider: CollisionShape3D = $Collider
 @onready var is_closed := !collider.disabled
 
 func set_closed(closed: bool):
 	is_closed = closed
 	collider.disabled = !closed
-	visible = closed
+	door_mesh.visible = closed
